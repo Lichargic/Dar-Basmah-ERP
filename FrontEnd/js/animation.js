1d54d8f7d8b4a1154f48b18ab059d3ea37bc2a67
Lichargic <amanmalik059@duck.com>
2025-05-25 20:53:45 +0400

    Moved index.html to Root; Updated Links to Absolute Paths

--- FrontEnd/js/animation.js
+++ FrontEnd/js/animation.js
@@ -1,13 +1,13 @@
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      entry.target.classList.add('show');
+      entry.target.classList.add("show");
     } else {
-      entry.target.classList.remove('show');
+      entry.target.classList.remove("show");
     }
   });
 });
 
 // Observe all sections on the page
-const sections = document.querySelectorAll('section');
+const sections = document.querySelectorAll("section");
 sections.forEach((section) => observer.observe(section));
